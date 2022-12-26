cafc0a35853de4609e85a5383f856cee09523b96
turtlecrane <108418994+turtlecrane@users.noreply.github.com>
2022-12-26 16:13:42 +0900

    팝업스타일 + 자동채굴 기능 추가

--- src/content.js
+++ src/content.js
@@ -5,74 +5,48 @@ if (typeof browser === "undefined") {
     var browser = chrome;
 }
 
-window.onload = function() {
+//포인트 자동 클릭
+function clickPoints() {
+	try {
+        // Get all clickable buttons inside 'community-points-summary'
+		let elems = document.querySelector('.community-points-summary').querySelectorAll('button');
+		
+		// Click each button, except for the first, which is the points spending menu
+		elems.forEach(function(currentElem, index, arr) {
+			if (index != 0) {
 
+                console.log('트위치 자동 채굴 : 클릭됨!');
+				currentElem.click();
+			}
+		});
+    }
+    catch(err) {}
 }
+setTimeout(function() {
+	console.log('트위치 자동 채굴 : 초기화됨.');
+	const ATTEMPT_NUM = 10;
+
+	(function loopie(i) {
+		setTimeout(function() {
+
+			console.log('트위치 자동 채굴 : Attempt ' + (11 - i) + '/' + ATTEMPT_NUM);
+
+			if (document.contains(document.getElementsByClassName('community-points-summary')[0])) {
+                console.log('트위치 자동 채굴 : 성공');
+
+				// Pre-check
+				clickPoints();
+				// React to creation of an element with the clicking points script
+				document.getElementsByClassName('community-points-summary').arrive('button', clickPoints);
+
+				i=1; // equivalent of break
+
+			}
+			if (--i) loopie(i);   //  decrement i and call myLoop again if i > 0
+		}, 10000)
+	})(ATTEMPT_NUM);
+
+}, 10000);
 
 
-//twitch area
-/** 
-
-const selector = "#live-page-chat > div > div > div > div > div > section > div > div.Layout-sc-nxg1ff-0.fwjUjn.chat-input > div:nth-child(2) > div.Layout-sc-nxg1ff-0.ejhEzS.chat-input__buttons-container > div.Layout-sc-nxg1ff-0.cwwMDL > div > div > div > div.Layout-sc-nxg1ff-0.Aqzax > div > div > div > button";
-function twitchBonus() {
-	if (window.location.href.includes("twitch.tv")) {
-        const bonusBox = document.querySelector(selector);
-        bonusBox != null ? bonusBox.click() : null;
-    }
-}
-
-setInterval(function() {
-    twitchBonus();
-    console.log("working");
-}, 7240);
-
-browser.runtime.onMessage.addListener(
-    function(response) {
-        if (response.type="highlight") {
-            const highLights = response.highLights;
-            const color = response.color;
-            let value=[];
-            window.localStorage.getItem("highLights")===null ? null : value = JSON.parse(window.localStorage.getItem("highLights"));
-            const temp = new Set([...highLights,...value]);
-            window.localStorage.setItem("highLights", JSON.stringify(Array.from(temp)));
-            window.localStorage.setItem("color", JSON.stringify(color));
-        }
-        else if (response.type="screenShot") {
-            if (window.location.href.includes("steamindiegame")) {
-
-            }
-        }
-    }
-);
-
-function highlighter() {
-    let highLights = [""];
-    let color = '';
-    window.localStorage.getItem("highLights")===null ? null : highLights = JSON.parse(window.localStorage.getItem("highLights"));
-    window.localStorage.getItem("color")===null ? null : color = window.localStorage.getItem("color").replaceAll('"','');
-    const chats = document.getElementsByClassName("chat-line__message");
-    for(let c of chats) {
-        try {
-            var nickname = c.getElementsByClassName("chat-author__intl-login")[0].innerHTML;
-        }
-        catch {
-            var nickname = c.getElementsByClassName("chat-author__display-name")[0].innerHTML;
-        }
-        nickname = nickname.slice(2,nickname.length-1);
-        for (let h of highLights) {
-            if (h==nickname) {
-                c.style.background=color;
-            }
-        }
-    }
-}
-
-setInterval(function() {
-    if (window.location.href.includes("twitch.tv")) {
-       highlighter();
-    }
-},400);
-
-
-
-*/
+//광고제거
